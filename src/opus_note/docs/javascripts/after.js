315616d673da097328503167e56979d69359c8cd
artmallo <wego_mao@hotmail.com>
2024-08-10 23:13:19 +0800

    upload - 202408102313

--- src/opus_note/docs/javascripts/after.js
+++ src/opus_note/docs/javascripts/after.js
@@ -1,4 +1,14 @@
 document$.subscribe(() => {
+    // 评论系统
+    Waline.init(
+        {
+            el: '#waline',
+            serverURL: 'https://my-notebook-waline-git-main-yiyifengheju.vercel.app/',
+            lang: 'zh-CN',
+            dark: '[data-md-color-scheme=slate]',
+        }
+    )
+
     // 石蒜模拟器
     var script = document.createElement('script');
     script.async = true;
@@ -7,4 +17,9 @@ document$.subscribe(() => {
 
     document.head.appendChild(script);
 
+    // 获取 Vanilla-LazyLoad 实例
+    var lazyLoadInstance = new LazyLoad();
+
+    // 调用 update() 方法来强制加载所有元素
+    lazyLoadInstance.update();
 });
--- src/opus_note/docs/javascripts/load_img.js
+++ src/opus_note/docs/javascripts/load_img.js
@@ -2,9 +2,11 @@ var lazyLoadInstance = new LazyLoad({
 // css属性选择器
   elements_selector: 'img',
 // 滚动多少加载
-  threshold: 0,
+//   threshold: 0,
 // 加载的元素URL的属性
   data_src: 'lazy-src',
-  preload:0
+  // preload:0
 })
+
+lazyLoadInstance.update()
 
